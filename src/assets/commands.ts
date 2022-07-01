1f408b872bc816e97946cadb794f10770f27f08b
DIR\pedro.j.sola.campoy <pedro.j.sola.campoy@accenture.com>
2022-07-01 11:23:27 +0200

    commands for demo

---
+++ src/assets/commands.ts
@@ -0,0 +1,6 @@
+// loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+// bootstrapApplication(AppComponent, {
+//     providers: [
+//       importProvidersFrom(RouterModule.forRoot(routes))
+//     ]
+//   })
